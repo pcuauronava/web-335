7f3953eea11f2ff55f1eccfc62b9c6d43df1ead3
Patrick Cuauro <pcuauronava@my365.bellevue.edu>
2023-05-31 23:31:34 -0400

    Comments and attribution included

--- week-2/cuauro_myworld.py
+++ week-2/cuauro_myworld.py
@@ -1,2 +1,11 @@
+# ==========================
+# Title: cuauro_myworld.py
+# Author: Richard Krasso
+# Modified by: Patrick Cuauro
+# Date: 31 May 2023
+# Description: Exercise 2.3
+# ==========================
+# variable declaration to store last name input from user
 last_name = input("What's your lastname? ")
+# show value of the variable
 print("You are now in " + last_name + " World!")
